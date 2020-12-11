de9f253399fea40dbc44b20f6f43e3a82e272db8
djemsJazz <dahmdjamel92@gmail.com>
2020-12-11 12:22:16 +0100

    adding getToken() method

--- src/Components/utils.js
+++ src/Components/utils.js
@@ -1,28 +1,41 @@
 import Axios from "axios"
 
-export const myHeaders = {
+const getHeaders = (token) => {
+  return {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "Application/json",
+    "Authorization": `Bearer ${token ? token : ""}`
+  }
 }
+
 export const baseApi = 'http://localhost:5000'
 
 export const apiFetch = async ({ url, method, data, headers }) => {
-    let requestConfig = { method: 'get', url: url }
-    if (method) {
-        requestConfig.method = method
-        requestConfig.data = data
-    }
-    try {
-        const requestHeaders = { ...myHeaders }
-        const request = Axios({
-            ...requestConfig,
-            baseURL: baseApi,
-            headers: requestHeaders,
-            timeout: 50000,
-            withCredentials: true
-        });
-        return request
-    } catch (error) {
-        console.log('Something wrong was happened', error)
-    }
+  let requestConfig = { method: 'get', url: url }
+  if (method) {
+    requestConfig.method = method
+    requestConfig.data = data
+  }
+  const token = getToken();
+  try {
+    const requestHeaders = { ...getHeaders(token) }
+    const request = Axios({
+      ...requestConfig,
+      baseURL: baseApi,
+      headers: requestHeaders,
+      timeout: 50000,
+      withCredentials: true
+    });
+    return request
+  } catch (error) {
+    console.log('Something wrong was happened', error)
+  }
+}
+
+const getToken = () => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return null;
+  }
+  return token;
 }
