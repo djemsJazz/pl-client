c6bf3ded5a2438cfd2ece9432eb9c9729764e451
djemsJazz <dahmdjamel92@gmail.com>
2020-12-11 13:55:28 +0100

    Removing unuses

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,8 @@
 import React, { useContext } from 'react';
-import { Button } from 'semantic-ui-react';
 import { LoginContext } from '../Context/LoginContext';
-import { apiFetch } from './utils';
 
 const Home = () => {
   const { user, isAuthenticated } = useContext(LoginContext);
-  const getUsers = async () => {
-    try {
-      const response = await apiFetch({ url: '/users' });
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <div className="container">
       <h1>Home page</h1>
@@ -29,7 +19,6 @@ const Home = () => {
         </p>
       </div>
       )}
-      <Button onClick={getUsers}>Fetch</Button>
     </div>
   );
 };
