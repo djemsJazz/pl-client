3a8ba05fc3df0c1d1f459f95c43b6633b5b4d7e9
djemsJazz <dahmdjamel92@gmail.com>
2020-12-11 12:21:37 +0100

    Removing unused functions

--- src/ApiRequests/User.js
+++ src/ApiRequests/User.js
@@ -1,25 +1,7 @@
-import Axios from 'axios'
-import { myHeaders, baseApi, apiFetch } from '../Components/utils'
+import {  apiFetch } from '../Components/utils'
 import { handlerError } from '../helpers/utils'
 
-export const signUpUser = async (data, cb, errCb) => {
-    try {
-        await Axios.post(`${baseApi}/users/signup`, data, { headers: myHeaders })
-        cb()
-    } catch (error) {
-       handlerError(error);
-        errCb()
-    }
-}
-export const verifyCode = async (data, cb, errCb) => {
-    try {
-        const { data: { token, user } } = await Axios.post(`${baseApi}/users/checkcode`, data, { headers: myHeaders })
-        cb(token, user)
-    } catch (error) {
-       handlerError(error);
-        errCb()
-    }
-}
+
 export const login = async (idToken, cb, errCb) => {
     try {
         const requestConfig = {method: 'POST', url: 'users/login', data: { idToken } };
@@ -31,7 +13,7 @@ export const login = async (idToken, cb, errCb) => {
         errCb()
     }
 }
-export const getCurrentUser = async (token, cb, errCb) => {
+export const getCurrentUser = async (cb, errCb) => {
     try {
         const requestConfig = {method: 'POST', url: 'users/current' };
         const response = await apiFetch(requestConfig);
